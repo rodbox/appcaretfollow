be809c1e5fdf3c33110c250b2719fc12432833d3
rodbox <rodolphe@rodbox.fr>
2014-12-17 10:51:03 +0100

    Update demo index.php

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+        <link type="text/css" rel="stylesheet" href="http://fonts.googleapis.com/css?family=Rock+Salt">
         <link rel="apple-touch-icon" href="images/ios_icon.png"/>
         <link rel="apple-touch-startup-image" href="images/ios_startup.png" />
     <link rel="stylesheet" href="http://demo.rodbox.fr/appdemo/css/style.css">
@@ -15,9 +16,10 @@
         
     </head>
     <body>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
+    <a href="https://github.com/rodbox/appcaretfollow"><img style="position: absolute; top: 0; left: 0; border: 0;" src="https://camo.githubusercontent.com/82b228a3648bf44fc1163ef44c62fcc60081495e/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f7265645f6161303030302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_left_red_aa0000.png"></a>
+        <div class="container page">
+
+                <div class="col-md-8 col-md-offset-2 top-2 line-3 fixed">
                    
     <h1>Demo</h1>
     <textarea class="caretposition" data-follower="#sub"></textarea>
@@ -28,14 +30,13 @@
         <a href="#" class="btn btn-xs c-4"><i class="glyphicon glyphicon-comment"></i></a></div>
     </div>
                 </div>
-                <div class="col-md-12">
-                   <div class="result"></div> 
-                </div>
-            </div>
+              
+
         </div>
-        <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
-        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
-        <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+        <script src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+    <script src="http://static.rodbox.fr/mobile-events/jquery.mobile-events.min.js"></script>
+    <script src="http://static.rodbox.fr/appmobile/jquery-appmobile.js"></script>
         <script src="jquery-app.js"></script>
         <script src="app.js"></script>
     </body>
